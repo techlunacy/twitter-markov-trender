5e8d6e2cb43bca5386972ca5e95272f4e0bcffdc
Graeme <graeme@techlunacy.com>
2015-08-22 09:20:49 +0000

    initial iteration through test strings and an attempt at getting the first parts of the string.the last part may not be nexesary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    let chain = ["this is a string", "this is another string", "is this a string?"];
+    let keys = chain.iter().flat_map(|s| s.split_whitespace().next()).collect();
 
+
+    for i in keys.iter() {
+        println!("{}", i);
+    }
+
+        println!("~~~~~~~~~~~~");
+    for i in chain.iter() {
+        for j in i.split_whitespace() {
+            println!("{}", j);
+        }
+    }
 }
